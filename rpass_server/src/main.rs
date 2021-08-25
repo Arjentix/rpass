b1014ed4d7fe9176dd0a91b49eac1c506896fac2
Arjentix <arjentix@gmail.com>
2021-08-25 17:42:51 +0300

    Moved to crossbeam

--- rpass_server/src/main.rs
+++ rpass_server/src/main.rs
@@ -1,29 +1,36 @@
+mod storage;
+
 use std::net::TcpListener;
 use std::io::Write;
 use std::borrow::Cow;
+use storage::Storage;
 
-use jod_thread::spawn; // To join thread on drop
-
-fn handle_client<T: Write>(mut stream: T) -> std::io::Result<()> {
+fn handle_client<T: Write>(mut stream: T, _storage: &Storage) -> std::io::Result<()> {
     stream.write_all("Hello from rpass server!".as_bytes())
 }
 
 fn main() -> std::io::Result<()> {
+    let storage = Storage::from_path("~/.rpass_storage");
+    let storage_ref = &storage;
+
     let listener = TcpListener::bind("127.0.0.1:3747")?;
-    let mut thread_handles = vec![];
 
-    for stream_res in listener.incoming() {
-        let stream = stream_res?;
-        let addr = match stream.peer_addr() {
-            Ok(peer_addr) => Cow::from(peer_addr.to_string()),
-            Err(_) => Cow::from("unknown")
-        };
-        println!("Connected with {}", addr);
+    crossbeam_utils::thread::scope(|spawner| {
+        for stream_res in listener.incoming() {
+            if let Err(_) = stream_res {
+                break;
+            }
 
-        thread_handles.push(
-            spawn(move || handle_client(stream))
-        );
-    }
+            let stream = stream_res.unwrap();
+            let addr = match stream.peer_addr() {
+                Ok(peer_addr) => Cow::from(peer_addr.to_string()),
+                Err(_) => Cow::from("unknown")
+            };
+            println!("Connected with {}", addr);
+
+            spawner.spawn(move |_| handle_client(stream, storage_ref));
+        }
+    }).unwrap();
 
     Ok(())
 }
