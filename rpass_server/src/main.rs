a4a053a729e44a706f75ce1ed248af0d23862483
Arjentix <arjentix@gmail.com>
2021-08-25 18:22:48 +0300

    Added async storage mutability

--- rpass_server/src/main.rs
+++ rpass_server/src/main.rs
@@ -3,15 +3,18 @@ mod storage;
 use std::net::TcpListener;
 use std::io::Write;
 use std::borrow::Cow;
+use std::sync::{Arc, RwLock};
 use storage::Storage;
 
-fn handle_client<T: Write>(mut stream: T, _storage: &Storage) -> std::io::Result<()> {
-    stream.write_all("Hello from rpass server!".as_bytes())
+fn handle_client<T: Write>(mut stream: T, storage: Arc<RwLock<Storage>>) -> std::io::Result<()> {
+    stream.write_all("Hello from rpass server!".as_bytes())?;
+    let mut storage_write = storage.write().unwrap();
+    storage_write.edit();
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
-    let storage = Storage::from_path("~/.rpass_storage");
-    let storage_ref = &storage;
+    let storage = Arc::new(RwLock::new(Storage::from_path("~/.rpass_storage")));
 
     let listener = TcpListener::bind("127.0.0.1:3747")?;
 
@@ -28,7 +31,8 @@ fn main() -> std::io::Result<()> {
             };
             println!("Connected with {}", addr);
 
-            spawner.spawn(move |_| handle_client(stream, storage_ref));
+            let storage_clone = storage.clone();
+            spawner.spawn(move |_| handle_client(stream, storage_clone));
         }
     }).unwrap();
 
--- rpass_server/src/storage.rs
+++ rpass_server/src/storage.rs
@@ -9,4 +9,8 @@ impl Storage {
     pub fn from_path<P: AsRef<Path> + ?Sized>(path: &P) -> Self {
         Storage {path: path.as_ref().to_path_buf()}
     }
+
+    pub fn edit(&mut self) {
+        self.path = PathBuf::from("/");
+    }
 }
