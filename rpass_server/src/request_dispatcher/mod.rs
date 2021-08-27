cd97300c09a5c493356c98762a41db8e9e4157e6
Arjentix <arjentix@gmail.com>
2021-08-27 17:57:19 +0300

    Handler removed by Fn

--- rpass_server/src/request_dispatcher/mod.rs
+++ rpass_server/src/request_dispatcher/mod.rs
@@ -4,25 +4,28 @@ pub trait Handler : Send + Sync {
     fn handle(&mut self, args : &mut dyn Iterator<Item=&str>) -> String;
 }
 
+type Callback = dyn Fn(&mut dyn Iterator<Item=&str>) -> String + Send + Sync;
+
 #[derive(Default)]
 pub struct RequestDispatcher {
-    command_to_handler: HashMap<String, Box<dyn Handler>>
+    command_to_callback: HashMap<String, Box<Callback>>
 }
 
 impl RequestDispatcher {
-    pub fn add_handler(&mut self, command: String, handler: Box<dyn Handler>) {
-        self.command_to_handler.insert(command, handler);
+    pub fn add_callback<C>(&mut self, command: String, callback: C)
+        where C: Fn(&mut dyn Iterator<Item=&str>) -> String + Send + Sync + 'static {
+        self.command_to_callback.insert(command, Box::new(callback));
     }
 
-    pub fn dispatch(&mut self, request: &String) -> Option<String> {
+    pub fn dispatch(&self, request: &String) -> Option<String> {
         let mut iter = request.split_whitespace();
         let command = match iter.next() {
             Some(cmd) => cmd,
             None => return None
         };
 
-        match self.command_to_handler.get_mut(command) {
-            Some(ref mut handler) => Some(handler.handle(&mut iter)),
+        match self.command_to_callback.get(command) {
+            Some(callback) => Some(callback(&mut iter)),
             None => None
         }
     }
