9beeda120af94022b3034e3e70930c55e4014937
Arjentix <arjentix@gmail.com>
2021-09-01 10:16:26 +0300

    Added Storage::get_user_pub_key()

--- rpass_server/src/storage.rs
+++ rpass_server/src/storage.rs
@@ -2,6 +2,9 @@ pub use rpass::key::Key;
 
 use std::path::{Path, PathBuf};
 use std::fs;
+use std::io::Result;
+
+const PUB_KEY_FILENAME: &'static str = "key.pub";
 
 /// Password storage
 pub struct Storage {
@@ -12,6 +15,7 @@ impl Storage {
     /// Initializes storage from given path to storage folder
     /// 
     /// # Panics
+    /// 
     /// Panics if can't open or create storage directory
     pub fn from_path<P: AsRef<Path>>(path: P) -> Self {
         let real_path = path.as_ref();
@@ -24,17 +28,32 @@ impl Storage {
     /// 
     /// Creates user folder with name `username` ans *key.pub* file with
     /// `pub_key` content. Makes no `username` validation
+    /// 
+    /// # Errors
+    /// 
+    /// Any errors during creating folder and writing file
     pub fn add_new_user(&mut self, username: &str, pub_key: &Key)
-            -> std::io::Result<()> {
+            -> Result<()> {
         let user_dir = self.path.join(username);
-        let pub_key_file = user_dir.join("key.pub");
+        let pub_key_file = user_dir.join(PUB_KEY_FILENAME);
         fs::create_dir(user_dir)?;
         fs::write(pub_key_file, pub_key.as_bytes())
+    }
+
+    /// Reads and returns user public key
+    /// 
+    /// # Errors
+    /// 
+    /// Any error during file reading
+    pub fn get_user_pub_key(&self, username: &str) -> Result<Key> {
+        let pub_key_file = self.path.join(username).join(PUB_KEY_FILENAME);
+        Key::from_bytes(&fs::read(pub_key_file)?)
     }
 
     /// Open storage directory
     /// 
     /// # Panics
+    /// 
     /// Panics if can't open or create storage directory
     fn open_storage(path: &Path) {
         const DIRECTORY_MESSAGE_PREFIX: &'static str =
