5bba0d6b0874d9c57c5393aab76423b6663aeab7
Arjentix <arjentix@gmail.com>
2021-09-19 17:58:07 +0300

    Added register callback tests

--- rpass_server/src/callbacks/register.rs
+++ rpass_server/src/callbacks/register.rs
@@ -52,19 +52,22 @@ pub fn register(storage: AsyncStorage, arg_iter: ArgIter)
 /// Checks if `username` is a valid string. 
 /// Valid means:
 /// * Not empty
-/// * All characters are ascii alphanumeric or `.` or `@`
+/// * All characters are ascii alphanumeric or `.`, or `@`, or `_`
 /// * Contains at least one alphabetic character
 /// * Doesn't contains `..`
-/// * Doesn't start with `.` or `@`
-/// * Doesn't end with `.` or `@`
+/// * Doesn't start with `.`, `@` or `_`
+/// * Doesn't end with `.`, `@` or `_`
 /// * No more than 32 characters in length
 fn is_valid_username(username: &str) -> bool {
     if username.is_empty() ||
-        !is_all_ascii_alphanumeric_or_dot_or_at_sign(username) ||
+        !username.chars().all(
+            |c| char::is_ascii_alphanumeric(&c) || c == '.' || c == '@' ||
+            c == '_') ||
         !username.chars().any(|c| char::is_ascii_alphabetic(&c)) ||
         is_contains_two_dots(username) ||
         username.starts_with('.') || username.starts_with('@') ||
-        username.ends_with('.') || username.ends_with('@') ||
+        username.starts_with('_') || username.ends_with('.') ||
+        username.ends_with('@') || username.ends_with('_') ||
         username.len() > 32 {
             return false;
     }
@@ -78,18 +81,80 @@ fn is_contains_two_dots(s: &str) -> bool {
         .any(|(c1, c2)| c1 == '.' && c2 == '.')
 }
 
-fn is_all_ascii_alphanumeric_or_dot_or_at_sign(s: &str) -> bool {
-    s.chars()
-        .all(|c| char::is_ascii_alphanumeric(&c) || c == '.' || c == '@')
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use std::io;
-    // use mockall::predicate;
+    use std::io;
+    use mockall::predicate;
 
-    // const TEST_USER: &'static str = "test_user";
+    #[test]
+    fn test_ok() {
+        let mock_storage = AsyncStorage::default();
+
+        const TEST_USER: &'static str = "test_user";
+        const KEY_STR: &'static str = "11:11";
+
+        mock_storage.write().unwrap().expect_add_new_user().times(1)
+        .with(predicate::eq(TEST_USER),
+            predicate::eq(Key::from_str(KEY_STR).unwrap()))
+        .returning(|_, _| Ok(()));
+
+        let args = TEST_USER.to_owned() + " " + KEY_STR;
+        let mut arg_iter = args.split_whitespace();
+        let res = register(mock_storage, &mut arg_iter);
+        assert_eq!(res.unwrap(), "Ok");
+    }
+
+    #[test]
+    fn test_empty_username() {
+        let mock_storage = AsyncStorage::default();
+
+        let mut arg_iter = "".split_whitespace();
+        let res = register(mock_storage, &mut arg_iter);
+        assert!(matches!(res, Err(RegistrationError::EmptyUsername)));
+    }
+
+    #[test]
+    fn test_invalid_username() {
+        let mock_storage = AsyncStorage::default();
+
+        const INVALID_USERNAME: &'static str = "_invalid_username_";
+        let mut arg_iter = INVALID_USERNAME.split_whitespace();
+
+        let res = register(mock_storage, &mut arg_iter);
+        assert!(matches!(res,
+            Err(RegistrationError::InvalidUsername(username))
+            if username == INVALID_USERNAME));
+    }
+
+    #[test]
+    fn test_empty_key() {
+        let mock_storage = AsyncStorage::default();
+
+        let mut arg_iter = "test_user".split_whitespace();
+        let res = register(mock_storage, &mut arg_iter);
+        assert!(matches!(res, Err(RegistrationError::EmptyKey)));
+    }
+
+    #[test]
+    fn test_invalid_key() {
+        let mock_storage = AsyncStorage::default();
+
+        let mut arg_iter = "test_user key".split_whitespace();
+        let res = register(mock_storage, &mut arg_iter);
+        assert!(matches!(res, Err(RegistrationError::InvalidKey(_))));
+    }
+
+    #[test]
+    fn test_user_already_exists() {
+        let mock_storage = AsyncStorage::default();
+
+        mock_storage.write().unwrap().expect_add_new_user().times(1)
+            .returning(|_, _| Err(io::Error::new(io::ErrorKind::AlreadyExists, "")));
+        let mut arg_iter = "test_user 11:11".split_whitespace();
+        let res = register(mock_storage, &mut arg_iter);
+        assert!(matches!(res, Err(RegistrationError::AlreadyExists(_))));
+    }
 
     #[test]
     fn test_is_valid_username() {
@@ -102,22 +167,11 @@ mod tests {
         assert!(!is_valid_username("user."));
         assert!(!is_valid_username("@user"));
         assert!(!is_valid_username("user@"));
+        assert!(!is_valid_username("_user"));
+        assert!(!is_valid_username("user_"));
         assert!(!is_valid_username(
             &String::from_utf8(vec![b'X'; 33]).unwrap()));
 
-        assert!(is_valid_username("user404@example.com"));
-    }
-
-    #[test]
-    fn test_ok() {
-        // let mock_storage = AsyncStorage::default();
-        // let mut session = Session::default();
-        // session.username = TEST_USER.to_owned();
-
-        // mock_storage.write().unwrap().expect_delete_user()
-        //     .with(predicate::eq(TEST_USER)).returning(|_|Ok(()));
-        // let res = delete_me(mock_storage, &mut session);
-        // assert!(res.is_ok());
-        // assert_eq!(res.unwrap(), "Ok".to_owned());
+        assert!(is_valid_username("user_404@example.com"));
     }
 }
