6b9858ce12b3321c466116b94912b7e23d3e9a94
Arjentix <arjentix@gmail.com>
2021-10-12 00:57:37 +0300

    Added list_records callback usage

--- rpass_server/src/main.rs
+++ rpass_server/src/main.rs
@@ -52,6 +52,7 @@ fn build_request_dispatcher(storage : Arc<RwLock<Storage>>)
         let delete_me_storage = storage.clone();
         let new_record_storage = storage.clone();
         let show_record_storage = storage.clone();
+        let list_records_storage = storage.clone();
 
         let mut dispatcher_write = request_dispatcher.write().unwrap();
         dispatcher_write
@@ -82,6 +83,10 @@ fn build_request_dispatcher(storage : Arc<RwLock<Storage>>)
         .add_callback(Cow::from("show_record"), move |session, arg_iter| {
             callbacks::show_record(
                 show_record_storage.clone(), session, arg_iter)
+                .map_err(|err| err.into())
+        })
+        .add_callback(Cow::from("list_records"), move |session, _| {
+            callbacks::list_records(list_records_storage.clone(), session)
                 .map_err(|err| err.into())
         });
     }
