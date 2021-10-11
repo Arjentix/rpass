75710a42c46a30ea9afed381a48850e5a0d1a8b3
Arjentix <arjentix@gmail.com>
2021-10-11 23:17:41 +0300

    show_record callback now checks resource name validity

--- rpass_server/src/callbacks/show_record.rs
+++ rpass_server/src/callbacks/show_record.rs
@@ -1,4 +1,4 @@
-use super::{Result, Error, AsyncStorage, Session, ArgIter};
+use super::{Result, Error, AsyncStorage, Session, ArgIter, utils};
 
 /// Shows record for resource from `arg_iter` for user `session.username`
 /// 
@@ -6,6 +6,7 @@ use super::{Result, Error, AsyncStorage, Session, ArgIter};
 /// 
 /// * `UnacceptableRequestAtThisState` - if not `session.is_authorized`
 /// * `EmptyResourceName` - if resource name wasn't provided
+/// * `InvalidResourceName` - if resource name is invalid
 /// * `StorageError` - if can't retrieve record cause of some error in
 /// `storage`
 pub fn show_record(storage: AsyncStorage, session: &Session, arg_iter: ArgIter)
@@ -15,6 +16,10 @@ pub fn show_record(storage: AsyncStorage, session: &Session, arg_iter: ArgIter)
     }
 
     let resource = arg_iter.next().ok_or(Error::EmptyResourceName)?;
+    if !utils::is_safe_for_filename(&resource) {
+        return Err(Error::InvalidResourceName);
+    }
+
     let storage_read = storage.read().unwrap();
     let record = storage_read.get_record(&session.username, &resource)?;
     Ok(record.to_string())
@@ -73,6 +78,21 @@ mod tests {
 
         assert!(matches!(show_record(mock_storage, &session, &mut arg_iter),
             Err(Error::EmptyResourceName)));
+    }
+
+    #[test]
+    fn test_invalid_resource() {
+        let mock_storage = AsyncStorage::default();
+        let session = Session {
+            is_authorized: true,
+            username : TEST_USER.to_owned(),
+            .. Session::default()
+        };
+        let args = ["./../resource.com".to_owned()];
+        let mut arg_iter = args.iter().cloned();
+
+        assert!(matches!(show_record(mock_storage, &session, &mut arg_iter),
+            Err(Error::InvalidResourceName)));
     }
 
     #[test]
