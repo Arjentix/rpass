023de0786d11b6b86bcd85792c705fd91b38f229
Arjentix <arjentix@gmail.com>
2021-10-31 22:40:44 +0300

    Session is enum now

--- rpass_server/src/session.rs
+++ rpass_server/src/session.rs
@@ -1,12 +1,40 @@
 use super::storage::UserStorage;
 use std::sync::{Arc, RwLock};
 
-/// Struct containing user session data
+/// Enum representing user session
+pub enum Session {
+    /// Initial state of every session
+    Unauthorized(Unauthorized),
+
+    /// Authorized session
+    Authorized(Authorized),
+
+    /// Session, that still exists, but has been ended
+    Ended
+}
+
 #[derive(Default)]
-pub struct Session {
-    pub is_authorized: bool,
+pub struct Unauthorized {
+    pub login_confirmation: String,
+}
+
+pub struct Authorized {
     pub username: String,
-    pub is_ended: bool,
-    pub login_confirmation: Option<String>,
-    pub user_storage: Option<Arc<RwLock<UserStorage>>>
+    pub user_storage: Arc<RwLock<UserStorage>>
+}
+
+impl Session {
+    /// Checks if session is authorized
+    pub fn is_authorized(&self) -> bool {
+        match self {
+            Authorized => true,
+            _ => false
+        }
+    }
+}
+
+impl Default for Session {
+    fn default() -> Self {
+        Session::Unauthorized(Unauthorized::default())
+    }
 }
