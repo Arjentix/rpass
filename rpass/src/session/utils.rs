773a6054932c4fc7742c0fb40282d726f29c697d
Arjentix <arjentix@gmail.com>
2021-12-28 23:00:33 +0300

    Added tests for utils functions

--- rpass/src/session/utils.rs
+++ rpass/src/session/utils.rs
@@ -10,8 +10,7 @@ use super::{Connector, Error, Result};
 pub fn read_good_response(connector: &mut Connector) -> Result<String> {
     let response = connector.recv_response()?;
 
-    const ERROR_PREFIX: &str = "Error: ";
-    if let Some(stripped) = response.strip_prefix(ERROR_PREFIX) {
+    if let Some(stripped) = response.strip_prefix("Error: ") {
         return Err(Error::Server {
             mes: stripped.to_string(),
         });
@@ -33,5 +32,83 @@ pub fn read_ok_response(connector: &mut Connector) -> Result<()> {
     match response {
         ok if ok == "Ok" => Ok(()),
         mes => Err(Error::UnexpectedResponse { response: mes }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod read_good_response {
+        use super::*;
+        use std::io;
+
+        #[test]
+        fn test_ok() {
+            let mut connector = Connector::default();
+            connector
+                .expect_recv_response()
+                .times(1)
+                .returning(|| Ok(String::from("Good job")));
+
+            assert_eq!(read_good_response(&mut connector).unwrap(), "Good job")
+        }
+
+        #[test]
+        fn test_cant_recv_response() {
+            let mut connector = Connector::default();
+            connector
+                .expect_recv_response()
+                .times(1)
+                .returning(|| Err(Error::Io(io::Error::new(io::ErrorKind::Other, ""))));
+
+            assert!(matches!(
+                read_good_response(&mut connector),
+                Err(Error::Io(_))
+            ))
+        }
+
+        #[test]
+        fn test_response_with_error() {
+            let mut connector = Connector::default();
+            connector
+                .expect_recv_response()
+                .times(1)
+                .returning(|| Ok(String::from("Error: test error")));
+
+            assert!(matches!(
+                read_good_response(&mut connector),
+                Err(Error::Server { mes }) if mes == "test error"
+            ))
+        }
+    }
+
+    mod read_ok_response {
+        use super::*;
+
+        #[test]
+        fn test_ok() {
+            let mut connector = Connector::default();
+            connector
+                .expect_recv_response()
+                .times(1)
+                .returning(|| Ok(String::from("Ok")));
+
+            read_good_response(&mut connector).unwrap();
+        }
+
+        #[test]
+        fn test_unexpected_response() {
+            let mut connector = Connector::default();
+            connector
+                .expect_recv_response()
+                .times(1)
+                .returning(|| Ok(String::from("Good")));
+
+            assert!(matches!(
+                read_ok_response(&mut connector),
+                Err(Error::UnexpectedResponse { response }) if response == "Good"
+            ))
+        }
     }
 }
