118107ffe417b8d592a7dc0ccf5abf59e60db401
Arjentix <arjentix@gmail.com>
2022-01-11 01:18:26 +0300

    utils were made async

--- rpass/src/session/utils.rs
+++ rpass/src/session/utils.rs
@@ -7,8 +7,8 @@ use super::{Connector, Error, Result};
 /// * `Io` - if can't write or read bytes to/from server
 /// * `InvalidResponseEncoding` - if response isn't UTF-8 encoded
 /// * `Server` - if server response contains error message
-pub fn read_good_response(connector: &mut Connector) -> Result<String> {
-    let response = connector.recv_response()?;
+pub async fn read_good_response(connector: &mut Connector) -> Result<String> {
+    let response = connector.recv_response().await?;
 
     if let Some(stripped) = response.strip_prefix("Error: ") {
         return Err(Error::Server {
@@ -27,8 +27,8 @@ pub fn read_good_response(connector: &mut Connector) -> Result<String> {
 /// * `InvalidResponseEncoding` - if response isn't UTF-8 encoded
 /// * `Server` - if server response contains error message
 /// * `UnexpectedResponse` - if response isn't *"Ok"* or error
-pub fn read_ok_response(connector: &mut Connector) -> Result<()> {
-    let response = read_good_response(connector)?;
+pub async fn read_ok_response(connector: &mut Connector) -> Result<()> {
+    let response = read_good_response(connector).await?;
     match response {
         ok if ok == "Ok" => Ok(()),
         mes => Err(Error::UnexpectedResponse { response: mes }),
@@ -43,19 +43,22 @@ mod tests {
         use super::*;
         use std::io;
 
-        #[test]
-        fn test_ok() {
+        #[tokio::test]
+        async fn test_ok() {
             let mut connector = Connector::default();
             connector
                 .expect_recv_response()
                 .times(1)
                 .returning(|| Ok(String::from("Good job")));
 
-            assert_eq!(read_good_response(&mut connector).unwrap(), "Good job")
+            assert_eq!(
+                read_good_response(&mut connector).await.unwrap(),
+                "Good job"
+            )
         }
 
-        #[test]
-        fn test_cant_recv_response() {
+        #[tokio::test]
+        async fn test_cant_recv_response() {
             let mut connector = Connector::default();
             connector
                 .expect_recv_response()
@@ -63,13 +66,13 @@ mod tests {
                 .returning(|| Err(Error::Io(io::Error::new(io::ErrorKind::Other, ""))));
 
             assert!(matches!(
-                read_good_response(&mut connector),
+                read_good_response(&mut connector).await,
                 Err(Error::Io(_))
             ))
         }
 
-        #[test]
-        fn test_response_with_error() {
+        #[tokio::test]
+        async fn test_response_with_error() {
             let mut connector = Connector::default();
             connector
                 .expect_recv_response()
@@ -77,7 +80,7 @@ mod tests {
                 .returning(|| Ok(String::from("Error: test error")));
 
             assert!(matches!(
-                read_good_response(&mut connector),
+                read_good_response(&mut connector).await,
                 Err(Error::Server { mes }) if mes == "test error"
             ))
         }
@@ -86,19 +89,19 @@ mod tests {
     mod read_ok_response {
         use super::*;
 
-        #[test]
-        fn test_ok() {
+        #[tokio::test]
+        async fn test_ok() {
             let mut connector = Connector::default();
             connector
                 .expect_recv_response()
                 .times(1)
                 .returning(|| Ok(String::from("Ok")));
 
-            read_good_response(&mut connector).unwrap();
+            read_good_response(&mut connector).await.unwrap();
         }
 
-        #[test]
-        fn test_unexpected_response() {
+        #[tokio::test]
+        async fn test_unexpected_response() {
             let mut connector = Connector::default();
             connector
                 .expect_recv_response()
@@ -106,7 +109,7 @@ mod tests {
                 .returning(|| Ok(String::from("Good")));
 
             assert!(matches!(
-                read_ok_response(&mut connector),
+                read_ok_response(&mut connector).await,
                 Err(Error::UnexpectedResponse { response }) if response == "Good"
             ))
         }
